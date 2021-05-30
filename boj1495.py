e3bc593bfa16ca5d548718eb8c95d76842c37ea9
Dagon <leedagon624@gmail.com>
2021-05-30 12:11:37 +0900

    Dynamic Programming

--- boj1495.py
+++ boj1495.py
@@ -2,7 +2,7 @@ n, s, m = map(int, input().split())
 v = list(map(int, input().split()))
 d = [[0] * (m + 1) for _ in range(n + 1)]
 d[0][s] = 1
-ans = - 1
+
 for i in range(n):
 	for j in range(m + 1):
 		if d[i][j] == 1:
@@ -10,7 +10,9 @@ for i in range(n):
 				d[i + 1][j - v[i]] = 1
 			if j + v[i] <= m:
 				d[i + 1][j + v[i]] = 1
-for i in range(m + 1):
+
+for i in range(m, -1, -1):
 	if d[n][i] == 1:
-		ans = i
-print(ans)
+		print(i)
+		exit()
+print(-1)
