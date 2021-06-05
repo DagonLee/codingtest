ca154b2a5c6da2c006f5e4d940288c0f565d9756
Dagon <leedagon624@gmail.com>
2021-06-05 12:50:13 +0900

    Queue/Simulation

--- boj1966.py
+++ boj1966.py
@@ -2,19 +2,19 @@ from collections import deque
 t = int(input())
 for _ in range(t):
 	n, idx = map(int, input().split())
-	imp_q = deque(list(map(int, input().split())))
-	idx_q = deque(range(n))
+	lst = list(map(int, input().split()))
+	q = deque()
+	for i, j in enumerate(lst):
+		q.append((i, j))
+	lst.sort()
 	cnt = 1
-	while True:
-		if imp_q[0] == max(imp_q):
-			if idx_q[0] == idx:
+	while q:
+		if q[0][1] == lst[-1]:
+			if q[0][0] == idx:
 				print(cnt)
 				break
 			cnt += 1
-			imp_q.popleft()
-			idx_q.popleft()
-			if not imp_q:
-				break
+			lst.pop()
+			q.popleft()
 		else:
-			imp_q.append(imp_q.popleft())
-			idx_q.append(idx_q.popleft())
+			q.append(q.popleft())
