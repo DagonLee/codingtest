d8742ca92c45a026bcf0002f10255e56a180fd74
Dagon <leedagon624@gmail.com>
2021-06-10 16:37:32 +0900

    TwoPointer

---
+++ boj1208.py
@@ -0,0 +1,17 @@
+n = int(input())
+a = list(map(int, input().split()))
+a.sort()
+left = 0
+right = n - 1
+ans = 2 * 1e9
+res = [()]
+while left <= right:
+	if left != right and abs(a[left] + a[right]) < abs(ans):
+		res.pop()
+		res.append((a[left], a[right]))
+		ans = a[left] + a[right]
+	if a[left] + a[right] < 0:
+		left += 1
+	else:
+		right -= 1
+print(res[0][0], res[0][1])
